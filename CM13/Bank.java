4f79dc6c7f4a3b69dc91a3b57bd03eb6768da2d9
Aldyth11 <aldyth.r.f@gmail.com>
2025-04-16 11:33:06 +0700

    Bank

--- CM13/Bank.java
+++ CM13/Bank.java
@@ -1,5 +1,26 @@
 package CM13;
 
 public class Bank {
-    
+    String norek;
+    String nama;
+    String namaIbu;
+    String nohp;
+    String email;
+
+    public Bank() {
+
+    }
+
+    public Bank(String norek, String nama, String namaIbu, String nohp, String email) {
+        this.norek = norek;
+        this.nama = nama;
+        this.namaIbu = namaIbu;
+        this.nohp = nohp;
+        this.email = email;
+    }
+
+    void tampilDataNorek() {
+        System.out.printf("%-15s %-10s %-10s %-17s %-20s\n", norek, nama, namaIbu, nohp, email);
+        
+    }
 }
